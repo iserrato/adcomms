efec8415a61b07106a2a5fcccd0153ee7f17f9bd
Isabel Serrato <iserrato@olin.edu>
2021-09-28 00:47:06 -0400

    Complete Analog Lab

--- Labs/Analog Lab/MATLAB Code/AnalogLabExercise2.m
+++ Labs/Analog Lab/MATLAB Code/AnalogLabExercise2.m
@@ -20,28 +20,61 @@ y_Q = imag(x)-mean(imag(x));
 
 %% Exercise 2
 clear
-load('Exercise2.mat')
+load('ex2_jeep.mat')
 
 %%
 
 t = [1:length(y_I)]*(1/fs);
+
+figure(1)
+plot(t, y_I);
+title('Time domain signal of y_I')
+xlabel('Time (seconds)')
+ylabel('Magnitude')
+
+figure(2)
+plot(t, y_Q);
+title('Time domain signal of y_Q')
+xlabel('Time (seconds)')
+ylabel('Magnitude')
+
 t_short = t(:, 1:end-1);
+t_sshort = t(:, 2:end-1);
 
 diff_yi = diff(y_I);
 diff_yq = diff(y_Q);
+
+figure(3)
+plot(t_short, diff_yi);
+title('Derivative of y_I')
+xlabel('Time (seconds)')
+ylabel('Magnitude')
+
+figure(4)
+plot(t_short, diff_yq);
+title('Derivative of y_Q')
+xlabel('Time (seconds)')
+ylabel('Magnitude')
+
 
 diff1 = diff_yq .* y_I(1:end-1,:);
 diff2 = diff_yi .* y_Q(1:end-1,:);
 diffs = diff1 - diff2;
 
-figure(7)
-plot(t_short, diffs);
-
 diffs_nm = diffs - mean(diffs);
 diffs_norm = diffs * 0.1 / max(abs(diffs));
 diffs_dec = decimate(diffs_norm, 4);
 
+figure(5)
+
+plot(t_short, diffs_norm);
+title('Demodulated Signal')
+xlabel('Time (seconds)')
+ylabel('Magnitude')
+
+%(122000:122300)
+
 % figure(8)
 % plot(t_short, diffs_norm);
 
-sound(diffs_dec, 300000/4)
+% sound(diffs_dec, 300000/4)
